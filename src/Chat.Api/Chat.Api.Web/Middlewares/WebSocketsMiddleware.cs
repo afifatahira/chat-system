d96fd5fb27a2475adbb69eed8763112513248ff3
Afifa Tahira <afifatahira@yahoo.com>
2022-03-25 11:44:47 +0600

    Chat application initialized

---
+++ src/Chat.Api/Chat.Api.Web/Middlewares/WebSocketsMiddleware.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace Chat.Api.Web.Middlewares
+{
+    public class WebSocketsMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public WebSocketsMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            var request = httpContext.Request;
+
+            if (request.Path.StartsWithSegments("/chatsocket", StringComparison.OrdinalIgnoreCase) &&
+            request.Query.TryGetValue("userId", out var appUserId))
+            {
+                request.Headers.Add("x-user-id", appUserId);
+            }
+
+            await _next(httpContext);
+        }
+    }
+}
